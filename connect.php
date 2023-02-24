df9b093dd38f71967eaf42ea61e1814e0c08048f
SpencerP21 <spencer.postlethwaite21@bathspa.ac.uk>
2023-02-24 18:18:19 +0000

    PHP DB Link
    +Linked the site to a database via Php to store information from the user

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+    $fname = $_POST['firstName']; // Transfers ID Data From HTML
+    $lname = $_POST['lastName'];
+    $DoB = $_POST['dateOfBirth'];
+    $pwd = $_POST['password'];
+
+    // Connect To Database
+    $conn = new mysqli('localhost', 'root','', 'engineering website');
+    if($conn->connect_error){ // If Statement Handling If The Database Isnt Connected
+        die('Connection Failed  :  ' .$conn->connect_error);      
+    }else { // Else Statement Inputting Data Into The Database
+        $stmt = $conn->prepare("insert into registration(firstName, lastName, dateOfBirth, password)
+            values(?, ?, ?, ?)");
+        $stmt->bind_param("ssss",$fName, $lName, $DoB, $pwd);
+        $stmt->execute();
+        echo "Account Retrieved Successfully...";
+        $stmt->close();
+        $conn->close();
+    }
+
+
+?>
